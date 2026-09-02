02d0703f3434e6093d12ca683eac5829fe823944
John Henry <jonhh.0023@gmail.com>
2026-09-01 22:32:22 -0400

    feat(internships): add PublishedInternship type definition

---
+++ frontend-pasantitas/src/features/internships/types/published-internship-interface.ts
@@ -0,0 +1,11 @@
+export interface PublishedInternship{
+    title: string,
+    description?: string;
+    location: string;
+    workMode: string;
+    postedAt: string;
+    salary: string;
+    duration: string;
+    area: string;
+    applicants: number;
+}
